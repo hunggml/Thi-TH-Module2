084495a1892a8dd1afcd31181e9c76add1c48297
dvhungg123@gmail.com <dvhungg123@gmail.com>
2020-11-11 10:38:46 +0700

    upload image background

--- index.php
+++ index.php
@@ -18,3 +18,29 @@ switch ($page) {
     default:
         $productController->show();
 }
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>HOME</title>
+</head>
+<body>
+<style>
+    body {
+        background-size: cover;
+        background-attachment: fixed;
+        background-image: url("image/backgroundTH.jpg");
+    }
+
+    img {
+        max-width: 66%;
+        height: auto;
+    }
+
+</style>
+</body>
+</html>
